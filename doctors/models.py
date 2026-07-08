9e5db7fca3902ad7594ec052f2a82a74f90481b8
unknown <hammermikuzen@gmail.com>
2026-07-08 21:40:42 +0800

    created and finished doctors app

--- doctors/models.py
+++ doctors/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Doctor(models.Model):
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="doctor"
+    )
+
+    license_number = models.CharField(
+        max_length=100,
+        unique=True
+    )
+
+    specialization = models.CharField(
+        max_length=100
+    )
+
+    availability_status = models.CharField(
+        max_length=50,
+        default="Available"
+    )
+
+    def __str__(self):
+        return self.user.get_full_name()
