a4ae0f7b72079b3c4b815afcba7670bd97fe9007
unknown <hammermikuzen@gmail.com>
2026-07-08 21:36:25 +0800

    finished staff (Clinic Staff entity) app

--- staff/models.py
+++ staff/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class ClinicStaff(models.Model):
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="clinic_staff"
+    )
+
+    date_hired = models.DateField()
+
+    schedule = models.CharField(
+        max_length=255,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.user.get_full_name()
